71beeb66a999a7483f886b45a1ccabe138a25c2b
邢迎坤 <11475112+xingyingkun@user.noreply.gitee.com>
2024-03-29 18:22:14 +0800

    111

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 export default async function handler(req, res) {
-  return axios.post("https://upload-api.anytools.me/upload", req, {
-    maxContentLength: Infinity,
-    maxBodyLength: Infinity,
-  });
+  console.log(req);
+  axios.post("https://upload-api.anytools.me/upload", req, {
+  }).then((response) => {
+    res.status(200).json(response.data);
+  })
 }
