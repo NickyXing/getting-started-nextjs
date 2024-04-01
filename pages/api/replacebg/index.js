9a169374ca816f759b5e3eae47e7249a90cb6ee9
邢迎坤 <11475112+xingyingkun@user.noreply.gitee.com>
2024-04-01 18:56:17 +0800

    更新removebg的模型

--- pages/api/replacebg/index.js
+++ pages/api/replacebg/index.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   const prediction = await replicate.predictions.create({
     // Pinned to a specific version of Stable Diffusion
     // See https://replicate.com/stability-ai/sdxl
-    version: "95fcc2a26d3899cd6c2691c900465aaeff466285a65c14638cc5f36f34befaf1",
+    version: "fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
 
     // This is the text prompt that will be submitted by a form on the frontend
     input: req.body.input,
